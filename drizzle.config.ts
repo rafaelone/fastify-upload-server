d539485b294bae30e127f8b166782d1712814411
Rafael <rafael.sergio@nextpage.com.br>
2025-01-21 16:41:28 -0300

    test: upload image

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default {
     url: env.DATABASE_URL,
   },
   dialect: 'postgresql',
-  schema: 'src/infra/db/schemas/*',
-  out: 'src/infra/db/migrations',
+  schema: 'src/db/schemas/*',
+  out: 'src/db/migrations',
 } satisfies Config
