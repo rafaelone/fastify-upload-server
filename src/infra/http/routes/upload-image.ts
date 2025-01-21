90d2db5072b09432ba700ad121bd0a3048aa3b26
Rafael <rafael.sergio@nextpage.com.br>
2025-01-21 15:23:53 -0300

    feat: upload files to r2

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -37,6 +37,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       })
 
       if (isRight(result)) {
+        console.log(unwrapEither(result))
         return reply.status(201).send()
       }
 
