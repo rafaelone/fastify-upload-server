6d27c353e8038d9c38ada389f616705480c3ba74
Rafael <rafael.sergio@nextpage.com.br>
2025-01-21 17:41:10 -0300

    feat: get uploads

--- src/db/schemas/uploads.ts
+++ src/db/schemas/uploads.ts
@@ -8,5 +8,5 @@ export const uploads = pgTable('uploads', {
   name: text('name').notNull(),
   remoteKey: text('remote_key').notNull().unique(),
   remoteUrl: text('remote_url').notNull(),
-  ceatedAt: timestamp('created_at').defaultNow().notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 })
